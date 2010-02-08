5709a65a806a259dc4f104d8e2263dba73f674b9
aaditya sood <a@ideadevice.com>
2010-02-08 09:28:47 +0530

    Add company and auto-complete mode

--- as-ui.el
+++ as-ui.el
@@ -402,5 +402,30 @@ The file cache can be saved to a file using
 (setq company-begin-commands '(self-insert-command))
 (setq company-idle-delay 0.2)
 
+(autoload 'company-mode "company" nil t)
+
+;;our sql completion
+(require 'as-sql-company)
+
+(require 'auto-complete)
+(require 'auto-complete-config)
+(global-auto-complete-mode t)
+
+(setq-default ac-sources '(ac-source-words-in-same-mode-buffers))
+(add-hook 'emacs-lisp-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-symbols)))
+(add-hook 'auto-complete-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-filename)))
+(global-auto-complete-mode t)
+(set-face-background 'ac-candidate-face "lightgray")
+(set-face-underline 'ac-candidate-face "darkgray")
+(set-face-background 'ac-selection-face "steelblue")
+(define-key ac-completing-map "\M-n" 'ac-next)
+(define-key ac-completing-map "\M-p" 'ac-previous)
+(setq ac-auto-start 2)
+(setq ac-dwim t)
+(define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
+
+(require 'ac-anything)
+(define-key ac-complete-mode-map (kbd "C-:") 'ac-complete-with-anything)
 
 (provide 'as-ui)
+
